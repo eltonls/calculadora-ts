895c6940ff3b16da5e63195e475d3cabfa48f06f
Elton <eltonrs1999@gmail.com>
2024-06-07 16:29:22 -0300

    add main class and methods

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-console.log("Main Function Test");
+import { Menu } from "./calculator/menu";
 
+class Main {
+    private m1: Menu
 
+    constructor() {
+        this.m1 = new Menu();
+    }
+
+    public main() {
+       this.m1.showMenu();
+    }
+}
+
+const main = new Main();
+
+main.main();
