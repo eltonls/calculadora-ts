ca9d1001de4a3c35d0d6e50a6adeca37e20bf7a3
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-06-12 13:01:08 -0300

    fix method call order in calculator (#6)

--- src/calculator/menu.ts
+++ src/calculator/menu.ts
@@ -2,7 +2,7 @@ import * as ReadLine from "readline-sync"
 import {Calculator} from "./calculator"
 
 
-class Menu{
+export class Menu{
     private calc :Calculator
     constructor(){
         this.calc = new Calculator()
@@ -11,16 +11,14 @@ class Menu{
     showMenu():void {
         let act: boolean = true
         while (act){
-
-            
-
             const op = ReadLine.question(`     
                    ------ FAÇA SUA OPERAÇÃO ------
             1 ) SOMA
             2 ) SUBTRAÇÃO
             3 ) MULTIPLICAÇÃO
             4 ) DIVISÃO
-            0 ) SAIR`)
+            0 ) SAIR
+            `)
 
             switch(op) { 
                 case '1': { 
@@ -34,27 +32,27 @@ class Menu{
                 case '2': { 
                     const a = ReadLine.question("Entre com o primeiro valor da multiplicação:")
                     const b = ReadLine.question("Entre com o segundo valor da multiplicação:")
-                    const result = this.calc.mult(Number(a),Number(b))
+                    const result = this.calc.sub(Number(a),Number(b))
                     console.log(result);
                    break; 
                 } 
                 case '3': { 
                     const a = ReadLine.question("Entre com o primeiro valor da divisão:")
                     const b = ReadLine.question("Entre com o segundo valor da divisão:")
-                    const result = this.calc.division(Number(a),Number(b))
+                    const result = this.calc.mult(Number(a),Number(b))
                     console.log(result);
                     break; 
                  } 
                  case '4': { 
                     const a = ReadLine.question("Entre com o primeiro valor da subtração:")
                     const b = ReadLine.question("Entre com o segundo valor da subtração:")
-                    const result = this.calc.sub(Number(a),Number(b))
+                    const result = this.calc.division(Number(a),Number(b))
                     console.log(result);
                     break; 
                  }
                  case '0':{
                     console.log("ENCERRANDO ...");
-                    
+
                     act = false
 
                     break
@@ -66,11 +64,6 @@ class Menu{
                    break; 
                 } 
              } 
-
-
-
-
-
         }
         
     }
